4b859bd2761993b6cbb87093fad81c98dbd8b7f3
Sofya <murz.sophie@gmail.com>
2021-12-15 03:56:52 +0100

    commit #3: add save and printer to InventoryManagerImpl

--- src/system/impl/InventoryManagerImpl.java
+++ src/system/impl/InventoryManagerImpl.java
@@ -1,12 +1,18 @@
 package system.impl;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import datamodel.Article;
+import datamodel.Currency;
 import datamodel.Order;
 import system.DataRepository;
+import system.Formatter;
+import system.Formatter.TableFormatter;
 import system.InventoryManager;
 
 class InventoryManagerImpl implements InventoryManager {
@@ -76,14 +82,59 @@ class InventoryManagerImpl implements InventoryManager {
 
 	@Override
 	public StringBuffer printInventory() {
-		// TODO Auto-generated method stub
-		return null;
+		return printInventory(0, true);
 	}
 
 	@Override
 	public StringBuffer printInventory(int sortedBy, boolean decending, Integer... limit) {
-		// TODO Auto-generated method stub
-		return null;
+		Stream<Article> articleStream = StreamSupport.stream(articleRepository.findAll().spliterator(), false);
+		Stream<Article> articleStreamSort;
+		//
+		if (sortedBy == 1){
+			articleStreamSort = articleStream.sorted(Comparator.comparingLong(p -> p.getUnitPrice()));
+		} else if (sortedBy == 2){
+			articleStreamSort =articleStream.sorted(Comparator.comparingLong(p -> p.getUnitPrice() * getUnitsInStock(p.getId())));
+		}else if (sortedBy == 3){
+			articleStreamSort = articleStream.sorted(Comparator.comparingInt(u -> getUnitsInStock(u.getId())));
+		}else if(sortedBy == 4){
+			articleStreamSort = articleStream.sorted(Comparator.comparing(u -> u.getDescription()));
+		} else if(sortedBy == 5){
+			articleStreamSort = articleStream.sorted(Comparator.comparing(u -> u.getId()));
+		}else{
+			articleStreamSort = StreamSupport.stream(articleRepository.findAll().spliterator(), false);
+		}
+		
+		Formatter formatter = new FormatterImpl();
+		TableFormatter tfmt = new TableFormatterImpl(formatter, new Object[][] {
+				// five column table with column specs: width and alignment ('[' left, ']'
+				// right)
+				{ 12, '[' }, { 32, '[' }, { 12, ']' }, { 10, ']' }, { 14, ']' }
+		})
+				.liner("+-+-+-+-+-+") // print table header
+				.hdr("||", "Inv.-Id", "Article / Unit", "Unit", "Units", "Value")
+				.hdr("||", "", "", "Price", "in-Stock", "(in €)")
+				.liner("+-+-+-+-+-+");
+		//
+		long totalValue = articleStreamSort
+				.map(a -> {
+					long unitsInStock = this.inventory.get(a.getId()).intValue();
+					long value = a.getUnitPrice() * unitsInStock;
+					tfmt.hdr("||",
+							a.getId(),
+							a.getDescription(),
+							formatter.fmtPrice(a.getUnitPrice(), a.getCurrency()).toString(),
+							Long.toString(unitsInStock),
+							formatter.fmtPrice(value, a.getCurrency()).toString());
+					return value;
+				})
+				.reduce(0L, (a, b) -> a + b);
+		//
+		String inventoryValue = formatter.fmtPrice(totalValue, Currency.EUR).toString();
+		tfmt
+				.liner("+-+-+-+-+-+")
+				.hdr("", "", "Invent", "ory Value:", inventoryValue);
+		//
+		return tfmt.getFormatter().getBuffer();
 	}
 
 	@Override
@@ -105,9 +156,20 @@ class InventoryManagerImpl implements InventoryManager {
 	}
 
 	@Override
-	public Article save(Article entity) {
-		// TODO Auto-generated method stub
-		return null;
+	public Article save(Article article) {
+		if (article == null)
+			throw new IllegalArgumentException("illegal article: null");
+		//
+		String id = article.getId();
+		if (id == null)
+			throw new IllegalArgumentException("illegal article.id: null");
+		//
+		articleRepository.save(article); // save, make sure to avoid duplicates
+		//
+		if (!inventory.containsKey(id)) {
+			this.inventory.put(id, Integer.valueOf(0));
+		}
+		return article;
 	}
 
 }
